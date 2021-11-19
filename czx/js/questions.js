8b84b086fcaef7b8ee545082fa20cda2ccd58223
ankit <ankitksingh1991@gmail.com>
2021-11-19 18:02:16 +0530

    final

---
+++ czx/js/questions.js
@@ -0,0 +1,60 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The correct sequence of HTML tags for starting a webpage is?",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Head, Body, TiTle"
+    ]
+  },
+    {
+    numb: 3,
+    question: "HTML uses?",
+    answer: "Fixed tags defined by the language",
+    options: [
+      "User defined tags",
+      "Pre-specified tags",
+      "Fixed tags defined by the language",
+      "Tags only for linking"
+    ]
+  },
+    {
+    numb: 4,
+    question: "HTML is what type of language?",
+    answer: "Markup Language",
+    options: [
+      "Scripting Language",
+      "Markup Language",
+      "Programming Language",
+      "Network Protocol"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What tag is used to display a picture in a HTML page?",
+    answer: "img",
+    options: [
+      "picture",
+      "image",
+      "img",
+      "src"
+    ]
+  },
+  
+  
+];
